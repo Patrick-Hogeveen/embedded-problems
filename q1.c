b3999d183dbd307ae7175763c5dea1d4f3be0a59
Patrick <patrick.hogeveen99@gmail.com>
2024-02-23 16:46:31 -0500

    cleaning code

--- q1.c
+++ q1.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-typedef struct node {
-    int val;
-    struct node * next;
-} node_t;
+
 
 int main() {
     int number1, number2, sum;
     scanf("%d %d", &number1, &number2);
-
-    node_t * head = NULL;
-    head = (node_t *) malloc(sizeof(node_t));
 
     if (number1 == number2) {
         printf("%dx%d \n", number1, number1);
@@ -28,11 +22,5 @@ int main() {
         }
     }
     printf("\n");
-    return 0;
-    //calc sum
-    sum = number1 + number2;
-
-    printf("%d + %d = %d \n", number1, number2, sum);
-
     return 0;
 }
